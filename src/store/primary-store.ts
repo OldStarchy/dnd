918eb15296a1f8d5d0c862603e7b658d7015e6ee
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-06-12 13:06:24 +0000

    feat: save/load primary-store to localstorage

--- src/store/primary-store.ts
+++ src/store/primary-store.ts
@@ -1,16 +1,25 @@
 import { configureStore, createListenerMiddleware } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from 'react-redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import initiativeReducer from './reducers/initiativeSlice';
 
 const listenerMiddlewareInstance = createListenerMiddleware();
 
+const persistConfig = {
+	key: 'dnd.primary-store',
+	storage,
+};
+
 export const primaryStore = configureStore({
 	reducer: {
-		initiative: initiativeReducer,
+		initiative: persistReducer(persistConfig, initiativeReducer),
 	},
 	middleware: (getDefaultMiddleware) =>
 		getDefaultMiddleware().prepend(listenerMiddlewareInstance.middleware),
 });
+
+export const persistor = persistStore(primaryStore);
 
 export type PrimaryState = ReturnType<typeof primaryStore.getState>;
 export type PrimaryDispatch = typeof primaryStore.dispatch;
