260e44888ffdb6e86535206b6b7d9bf554536065
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-06-14 04:11:52 +0000

    feat: add browser test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,49 @@
+/// <reference types="@vitest/browser/matchers" />
 /// <reference types="vitest/config" />
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vite';
+
+const aliasConfig = {
+	'@': path.resolve(__dirname, './src'),
+};
 
 export default defineConfig({
 	test: {
-		include: ['src/**/*.test.{ts,tsx}'],
-		exclude: ['src/generated/**'],
+		projects: [
+			{
+				test: {
+					name: 'unit',
+					environment: 'node',
+					include: ['src/**/*.test.ts'],
+					exclude: ['src/generated/**'],
+				},
+				resolve: {
+					alias: aliasConfig,
+				},
+			},
+			{
+				plugins: [react(), tailwindcss()],
+				test: {
+					name: 'browser',
+					environment: 'browser',
+					include: ['src/**/*.test.tsx'],
+					exclude: ['src/generated/**'],
+					css: true,
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: 'playwright',
+						// https://vitest.dev/guide/browser/playwright
+						instances: [{ browser: 'chromium' }],
+					},
+				},
+				resolve: {
+					alias: aliasConfig,
+				},
+			},
+		],
 		coverage: {
 			include: ['src/**/*.{ts,tsx}'],
 		},
