3e0275c161ce22dc5a9697e3bfabbf7dfe733406
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-07-12 03:18:00 +0000

    fix: ensure base_url's are normalized

--- src/hooks/useServerConfig.ts
+++ src/hooks/useServerConfig.ts
@@ -1,6 +1,9 @@
 import useLocalStorage from './useLocalStorage';
 
-const DEFAULT_SERVER_URL = import.meta.env.VITE_BASE_URL || '';
+const DEFAULT_SERVER_URL = (import.meta.env.VITE_BASE_URL || '').replace(
+	/\/+$/,
+	'',
+);
 
 export function useServerConfig(): [string, (url: string) => void] {
 	const [serverUrl, setServerUrl] = useLocalStorage(
