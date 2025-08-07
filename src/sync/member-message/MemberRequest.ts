ae42bf53c423eadd02404e61afd6bcb124b648d6
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-08-07 15:36:05 +0000

    wip: remove some unused types

--- src/sync/member-message/MemberRequest.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-// TODO: Replace with a more specific schema if possible
-export const memberRequestSchema = z.unknown().refine((x) => x !== undefined);
-
-export type MemberRequest = z.infer<typeof memberRequestSchema>;
