2925ed458b9fa8f4d13c9a374c6e18b3a5291051
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-08-07 15:36:05 +0000

    wip: remove some unused types

--- src/sync/host-message/HostNotification.ts
+++ src/sync/host-message/HostNotification.ts
@@ -1,34 +1,16 @@
-import { initiativeTableEntrySpec } from '@/components/InitiativeTable/InitiativeTableEntry';
+import { initiativeTableEntrySchema } from '@/components/InitiativeTable/InitiativeTableEntry';
 import { z } from 'zod';
 
 export const hostNotificationSpec = z.union([
 	z.object({
 		type: z.literal('initiativeTableUpdate'),
-		data: z.array(initiativeTableEntrySpec),
+		data: z.object({
+			entries: z.array(initiativeTableEntrySchema),
+			currentTurnId: z.string().nullable(),
+		}),
 	}),
 	z.object({
 		type: z.literal('heartbeat'),
-	}),
-	// System messages migrated to notifications
-	z.object({
-		type: z.literal('roomCreated'),
-		token: z.string(),
-		roomCode: z.string(),
-	}),
-	z.object({
-		type: z.literal('roomFound'),
-		roomCode: z.string(),
-	}),
-	z.object({
-		type: z.literal('roomJoined'),
-		token: z.string(),
-	}),
-	z.object({
-		type: z.literal('userJoined'),
-		token: z.string(),
-	}),
-	z.object({
-		type: z.literal('connectionReplaced'),
 	}),
 ]);
 
