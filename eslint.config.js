186eeb27c972c55d61f31b4a8377ffb70e0d5d2a
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2025-09-28 04:13:50 +0000

    style: add organize imports eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImportsPlugin from 'eslint-plugin-unused-imports';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default tseslint.config(
 	{ ignores: ['dist'] },
 	{
 		extends: [js.configs.recommended, ...tseslint.configs.recommended],
 		files: ['**/*.{ts,tsx}'],
+		ignores: ['dnd5eapi/**', 'src/components/ui/**'],
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
@@ -26,6 +29,43 @@ export default tseslint.config(
 			],
 			'@typescript-eslint/no-unused-vars': 'off',
 			'@typescript-eslint/no-namespace': 'off',
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'@typescript-eslint/no-import-type-side-effects': 'error',
+		},
+	},
+	{
+		plugins: {
+			import: importPlugin,
+			'unused-imports': unusedImportsPlugin,
+		},
+		settings: {
+			'import/internal-regex': '^@/',
+		},
+		rules: {
+			'unused-imports/no-unused-imports': 'error',
+			'import/enforce-node-protocol-usage': ['error', 'always'],
+			'import/first': 'error',
+			'import/newline-after-import': 'error',
+			'import/no-duplicates': 'error',
+			'import/no-useless-path-segments': 'error',
+			'import/no-namespace': 'error',
+			'import/order': [
+				'error',
+				{
+					groups: [
+						'builtin',
+						'external',
+						'internal',
+						['parent', 'sibling', 'index'],
+					],
+					'newlines-between': 'always',
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+					named: true,
+				},
+			],
 		},
 	},
 	eslintConfigPrettier,
